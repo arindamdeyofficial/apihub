c0b5fab4ba53326acb14de185bd54746cad5e2f7
Arindam Dey <biplabhome@gmail.com>
2020-05-10 15:32:47 +0530

    version log added

--- app.js
+++ app.js
@@ -2,9 +2,9 @@ var app = require('express')();
 var http = require('http').createServer(app);
 
 app.get('/', (req, res) => {
-  res.send('<h1>Hello world</h1>');
+  res.sendFile(__dirname + '/index.html');
 });
-
+console.log(process.version);
 http.listen(3000, () => {
   console.log('listening on *:3000');
 });
