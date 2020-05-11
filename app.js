ff38092e45dbb48dadd77a99564f46c7190ba678
Arindam Dey <biplabhome@gmail.com>
2020-05-11 14:13:01 +0530

    socket.io added

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const port = process.env.PORT || 3000;
 var app = require('express')();
 var http = require('http').createServer(app);
+var io = require('socket.io');
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
