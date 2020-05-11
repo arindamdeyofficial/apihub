10e2754dbe04d5bbf4ec47e19c7b1dc03c88df0d
Arindam Dey <biplabhome@gmail.com>
2020-05-11 14:30:42 +0530

    CORS

--- app.js
+++ app.js
@@ -10,3 +10,23 @@ console.log(process.version);
 http.listen(port, () => {
   console.log('listening on *:3000');
 });
+
+// Add headers
+app.use(function (req, res, next) {
+
+  // Website you wish to allow to connect
+  res.setHeader('Access-Control-Allow-Origin', 'https://apihubnode.azurewebsites.net/socket.io');
+
+  // Request methods you wish to allow
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+  // Request headers you wish to allow
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+  // Set to true if you need the website to include cookies in the requests sent
+  // to the API (e.g. in case you use sessions)
+  res.setHeader('Access-Control-Allow-Credentials', true);
+
+  // Pass to next layer of middleware
+  next();
+});
